fcd29d2c2235fb4ad65c44159d2c6350872193f7
FLWallace105 <fwallace@fambrands.com>
2020-12-29 14:11:38 -0800

    january 2021 monthly charging 1-5

--- background_allocation_helper.rb
+++ background_allocation_helper.rb
@@ -267,15 +267,15 @@ module BackgroundHelper
         contains_outlier_size = false
         my_size_hash.each do |key, value|
             puts "#{key}, #{value}"
-            if (value == "XS")
-                contains_outlier_size = true
-            end
-            if  (value == "S") 
-                contains_outlier_size = true
-            end
-            if  (value == "M") 
-                contains_outlier_size = true
-            end
+            #if (value == "XS")
+            #    contains_outlier_size = true
+            #end
+            #if  (value == "S") 
+            #    contains_outlier_size = true
+            #end
+            #if  (value == "M") 
+            #    contains_outlier_size = true
+            #end
 
         end
         return contains_outlier_size
@@ -344,7 +344,7 @@ module BackgroundHelper
             when 4
                 my_size_hash.delete("sports-bra")
             when 5
-                my_size_hash.delete("tops")
+                my_size_hash.delete("sports-jacket")
             when 6
                 my_size_hash.delete("sports-bra")
             when 7
@@ -523,14 +523,14 @@ module BackgroundHelper
                 contains_outlier = determine_outlier_sizes(my_size_hash)
                 if contains_outlier
                     puts "must generate only random 1-4"
-                    my_total_length = 6
+                    my_total_length = 5
                     my_index = generate_random_index(my_total_length)
                     #temp fix to make namaste & Slay work only add in March allocation 2020
                     #my_index = 7
                     puts "my_index = #{my_index}"
                 else
                     puts "can generate random 1-5"
-                    my_total_length = 7
+                    my_total_length = 5
                     my_index = generate_random_index(my_total_length)
                     
                     #my_index = 7
